ec8c3c399b1e7f9e8276f6de246bffeee0362946
Olexandr Z <zeeelensky@gmail.com>
2022-04-18 18:57:41 +0300

    Add project files.

---
+++ ToDoAppDB/Tasks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Tasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[TaskName] VARCHAR(50),
+	[TaskDescription] VARCHAR(50) NOT NULL,
+	[Deadline] DATETIME, 
+	[IsDone] BIT,
+	[Category] VARCHAR(30)
+)
